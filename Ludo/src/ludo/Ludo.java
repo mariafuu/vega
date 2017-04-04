b266508f81a87d56d1d8591499657430fb635bd7
mariafuu <maria.fuglemsmo@gmail.com>
2017-04-04 11:12:56 +0200

    Started work on a start screen where users can start a game of ludo

--- Ludo/src/ludo/Ludo.java
+++ Ludo/src/ludo/Ludo.java
@@ -1,21 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Main class of Ludo. Shows a start Scene where players can start the game.
  */
 package ludo;
+
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+
 
 /**
  *
  * @author maria
  */
-public class Ludo {
+public class Ludo extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    @Override
+    public void start (Stage startStage) {
+        startStage.setTitle("Ludo");
+        
+        Group root = new Group();
+        Scene startScene = new Scene(root);
+        startStage.setScene(startScene);
+        
+        Canvas startCanvas = new Canvas(400, 200);
+        root.getChildren().add(startCanvas);
+        
+        GraphicsContext gc = startCanvas.getGraphicsContext2D();
+        
+        startStage.show();
     }
     
 }
